fde2141657c5686e0aa422ff41b1dd45f355d654
Konrad Bieniek <konradbieniek@gmail.com>
2022-07-08 15:27:43 +0200

    completed project

--- script.js
+++ script.js
@@ -34,6 +34,8 @@ const prepareDOMEvents = () => {
     $addBtn.addEventListener('click', addNewTask);
     $todoInput.addEventListener('keyup', enterCheck)
     $ulList.addEventListener('click', checkClick)
+    $addPopupBtn.addEventListener('click', changeTodo)
+    $closeTodoBtn.addEventListener('click', closePopup)
 }
 
 const addNewTask = () => {
@@ -67,15 +69,42 @@ const checkClick = e => {
             e.target.closest('li').classList.toggle('completed')
             e.target.closest('button').classList.toggle('completed')
         } else if (e.target.closest('button').classList.contains('edit')) {
-            console.log('edit');
+            editTask(e)
         } else if (e.target.closest('button').classList.contains('delete')) {
             deleteTask(e)
         }
     }
 }
 
-const deleteTask = e => {
+const editTask = (e) => {
+    const oldTodo = e.target.closest('li').id
+    $editedTodo = document.getElementById(`${oldTodo}`)
+    $popup.style.display = 'flex'
+    $popupInput.value = $editedTodo.firstChild.textContent
+}
 
+const changeTodo = () => {
+    if ($popupInput.value !== '') {
+        $popupInfo.textContent = ''
+        $popup.style.display = 'none'
+        $editedTodo.firstChild.textContent = $popupInput.value
+
+    } else {
+        $popupInfo.innerText = 'Musisz wpisać treść.'
+    }
+}
+
+const closePopup = () => {
+    $popup.style.display = 'none'
+    $popupInfo.innerText = ''
+}
+
+const deleteTask = e => {
+    const deleteTodo = e.target.closest('li')
+    deleteTodo.remove();
+    if ($allTasks.length === 0) {
+        $alertInfo.innerText = 'Brak zadań na liście.';
+    }
 }
 
 document.addEventListener('DOMContentLoaded', main);
